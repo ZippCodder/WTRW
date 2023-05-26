4306fbd648507fca83757bb85e81d8840bf60478
deonrich <deon.edward.rich@gmail.com>
2023-05-26 10:46:36 -0600

    fixed issues with target invertion and pther tings

--- src/lib.js
+++ src/lib.js
@@ -306,8 +306,10 @@ export function genObjectId(length = 10) {
     return res.join("");
 }
 
-export function random(max) {
-    return Math.floor(Math.random() * (max - 0)) + 0;
+export function random(max,mirror) {
+   let n =  Math.floor(Math.random() * (max - 0)) + 0;
+   
+   return (!mirror) ? n:(Math.random() < 0.5) ? n:-n;
 }
 
 // Animation creation objects
